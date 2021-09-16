d3c3fe6c65b98e7037a9ce62133c5b17575eb493
julkar nain <kmjulkarnain@gmail.com>
2021-09-16 15:53:15 +0600

    Add minHeap

--- src/main/kotlin/heap/MinHeap.kt
+++ src/main/kotlin/heap/MinHeap.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 @author "julkar nain"
 @since 9/16/21 12:00 PM
 */
-class MaxHeap(n: Int) {
+class MinHeap(n: Int) {
 
     private var heap: IntArray = IntArray(n) { Int.MIN_VALUE }
     var heapSize = 0
@@ -33,28 +33,28 @@ class MaxHeap(n: Int) {
     private fun insertHeapify(i: Int) {
         val parent = i.parent()
 
-        if (heap[i] > heap[parent]) {
+        if (heap[i] < heap[parent]) {
             swap(i, parent)
             insertHeapify(parent)
         }
     }
 
     private fun deleteHeapify(i: Int) {
-        var largest = i
+        var smallest = i
         val left = i.left()
         val right = i.right()
 
-        if (left < heapSize && heap[left] > heap[largest]) {
-            largest = left
+        if (left < heapSize && heap[left] < heap[smallest]) {
+            smallest = left
         }
 
-        if (right < heapSize && heap[right] > heap[largest]) {
-            largest = right
+        if (right < heapSize && heap[right] < heap[smallest]) {
+            smallest = right
         }
 
-        if (largest != i) {
-            swap(i, largest)
-            deleteHeapify(largest)
+        if (smallest != i) {
+            swap(i, smallest)
+            deleteHeapify(smallest)
         }
     }
 
@@ -83,22 +83,22 @@ class MaxHeap(n: Int) {
     private fun Int.right() = 2 * this + 2
 }
 
-class MaxHeapTest {
+class MinHeapTest {
 
     @Test
     fun test() {
-        val heap = MaxHeap(6)
+        val heap = MinHeap(6)
         heap.insert(3)
         heap.insert(2)
         heap.insert(15)
         heap.insert(4)
         heap.insert(14)
         heap.insert(45)
-        assertEquals(listOf(45, 14, 15, 2, 4, 3), heap.getHeapData())
+        assertEquals(listOf(2, 3, 15, 4, 14, 45), heap.getHeapData())
         heap.delete()
         heap.delete()
-        assertEquals(listOf(14, 4, 3, 2, 15, 45), heap.getHeapData())
+        assertEquals(listOf(4, 14, 15, 45, 3, 2), heap.getHeapData())
         assertEquals(4, heap.heapSize)
-        assertEquals(listOf(2, 3, 4, 14, 15, 45), heap.sort())
+        assertEquals(listOf(45, 15, 14, 4, 3, 2), heap.sort())
     }
 }
